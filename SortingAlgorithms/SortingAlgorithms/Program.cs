8a41eafdbff657c544f02f4e1b56d4529a7a9699
Joshua Fischer <joshuadf@ymail.com>
2016-08-25 14:22:30 -0400

    Cleaned up the UI and added Array.Sort to compare the insertion sort vs the built in sorting class.

--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -8,29 +8,42 @@ namespace SortingAlgorithms
 {
     class Program
     {
-
+        static int[] unsortedArray;
         static void Main(string[] args)
         {
-            int[] unsortedArray;
-            Random rnd1 = new Random();
-
-            unsortedArray = new int[1000];
-            for(int i = 0; i < unsortedArray.Length; i++)
-            {
-                unsortedArray[i] = rnd1.Next(0,1000);
-            }
-
             var watch = System.Diagnostics.Stopwatch.StartNew();
             InsertionSort ISort = new InsertionSort(unsortedArray);
-            watch.Stop(); 
+            watch.Stop();
+
             
-            //Console.WriteLine("Sorted Array with Insertion Sort: " + ISort.ToString());
-            Console.WriteLine("Time spent to create object and sort the array: " + watch.Elapsed);
-            Console.WriteLine("Ticks spent to create object and sort the array: " + watch.Elapsed.Ticks);
+            Console.WriteLine("Unsorted int array size of {0}", unsortedArray.Length);
+            Console.WriteLine("Insertion Sort - Ticks/ Time in Milliseconds spent to create object and sort the array: " + watch.Elapsed.Ticks);
+
+            watch.Reset();
+            watch.Start();
+            Array.Sort(unsortedArray);
+            watch.Stop();
+
+           //This method uses the Array.Sort method, which applies the introspective sort as follows:
+          //•If the partition size is fewer than 16 elements, it uses an insertion sort algorithm.
+          //•If the number of partitions exceeds 2 * LogN, where N is the range of the input array, it uses a Heapsort algorithm.
+          //•Otherwise, it uses a Quicksort algorithm.
+
+            Console.WriteLine(".Net Sort - Ticks/ Time in Milliseconds spent to create object and sort the array: " + watch.Elapsed.Ticks);
+
 
             Console.ReadKey();
         }
 
-       
+       void buildUnsortedArray()
+        {
+            Random rnd1 = new Random();
+
+            unsortedArray = new int[10000];
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                unsortedArray[i] = rnd1.Next(0, 1000);
+            }
+        }
     }
 }
