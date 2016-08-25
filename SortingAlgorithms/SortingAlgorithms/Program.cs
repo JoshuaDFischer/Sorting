1b6423f5fdf92bb7288a372748c48a74d442bd20
Joshua Fischer <joshuadf@ymail.com>
2016-08-25 16:00:13 -0400

    Added new unsorted array and ran the sorts twice. Once for a big array and once for a small array.

--- SortingAlgorithms/SortingAlgorithms/Program.cs
+++ SortingAlgorithms/SortingAlgorithms/Program.cs
@@ -11,23 +11,53 @@ namespace SortingAlgorithms
         static int[] unsortedArray;
         static void Main(string[] args)
         {
-            buildUnsortedArray();
+            buildUnsortedArray(10);
+            RunSorts();
 
+            buildUnsortedArray(10000);
+            RunSorts();
+
+
+            Console.ReadKey();
+        }
+
+       static void buildUnsortedArray(int size)
+        {
+            Random rnd1 = new Random();
+            
+            unsortedArray = new int[size];
+            for (int i = 0; i < unsortedArray.Length; i++)
+            {
+                unsortedArray[i] = rnd1.Next(0, size);
+            }
+        }
+
+        static void RunSorts()
+        {
+            var watch = new System.Diagnostics.Stopwatch();
             Console.WriteLine("Unsorted int array size of {0}", unsortedArray.Length);
 
-            var watch = System.Diagnostics.Stopwatch.StartNew();
+            watch.Start();
             InsertionSort ISort = new InsertionSort(unsortedArray);
             watch.Stop();
-                        
+
             Console.WriteLine("Insertion Sort - Ticks/ Time in Milliseconds spent to create object and sort the array: " + watch.Elapsed.Ticks);
             watch.Reset();
 
             watch.Start();
             MergeSort MSort = new MergeSort(unsortedArray);
             watch.Stop();
-            
+
             Console.WriteLine("Merge Sort - Ticks/ Time in Milliseconds spent to create object and sort the array: " + watch.Elapsed.Ticks);
-            Console.WriteLine(MSort.ToString());
+
+            watch.Reset();
+
+            watch.Start();
+            QuickSort QSort = new QuickSort(unsortedArray);
+            watch.Stop();
+
+            Console.WriteLine("Quick Sort - Ticks/ Time in Milliseconds spent to create object and sort the array: " + watch.Elapsed.Ticks);
+            
 
 
             watch.Reset();
@@ -35,26 +65,13 @@ namespace SortingAlgorithms
             Array.Sort(unsortedArray);
             watch.Stop();
 
-           //This method uses the Array.Sort method, which applies the introspective sort as follows:
-          //•If the partition size is fewer than 16 elements, it uses an insertion sort algorithm.
-          //•If the number of partitions exceeds 2 * LogN, where N is the range of the input array, it uses a Heapsort algorithm.
-          //•Otherwise, it uses a Quicksort algorithm.
+            //This method uses the Array.Sort method, which applies the introspective sort as follows:
+            //•If the partition size is fewer than 16 elements, it uses an insertion sort algorithm.
+            //•If the number of partitions exceeds 2 * LogN, where N is the range of the input array, it uses a Heapsort algorithm.
+            //•Otherwise, it uses a Quicksort algorithm.
 
             Console.WriteLine(".Net Sort - Ticks/ Time in Milliseconds spent to create object and sort the array: " + watch.Elapsed.Ticks);
 
-
-            Console.ReadKey();
-        }
-
-       static void buildUnsortedArray()
-        {
-            Random rnd1 = new Random();
-
-            unsortedArray = new int[10000];
-            for (int i = 0; i < unsortedArray.Length; i++)
-            {
-                unsortedArray[i] = rnd1.Next(0, 1000);
-            }
         }
     }
 }
---
+++ SortingAlgorithms/SortingAlgorithms/QuickSort.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SortingAlgorithms
+{
+    class QuickSort
+    {
+        private int[] array;
+        public QuickSort(int[] inarray)
+        {
+            array = inarray;
+            quickSort(array, 0, array.Length -1);
+        }
+
+        public int[] Array
+        {
+            get
+            {
+                return array;
+            }
+        }
+
+        public void quickSort(int[] a, int lo, int hi)
+        {
+            if (lo < hi)
+            {
+                int p = partition(a, lo, hi);
+                quickSort(a, lo, p - 1);
+                quickSort(a, p + 1, hi);
+            }
+        }
+
+        int partition(int[] a, int lo, int hi)
+        {
+            int pivot = a[hi];
+            int i = lo;
+            for (int j = lo; j <= (hi - 1); j++)
+            {
+                if (a[j] <= pivot)
+                {
+                    int swap = a[i];
+                    a[i] = a[j];
+                    a[j] = swap;
+                    i++;
+                }
+            }
+            int temp = a[i];
+            a[i] = a[hi];
+            a[hi] = temp;
+            return i;
+        }
+
+        public override string ToString()
+        {
+            string builder = "";
+            int i = 0;
+            do
+            {
+                builder += array[i] + ",";
+                i++;
+            }
+            while (i < array.Length);
+
+            return builder;
+        }
+    }
+}
